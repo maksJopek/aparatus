4582aee9c51800f7b236fc9d731829c16254429f
Maksymilian Jopek <maks@jopek.eu>
2023-03-28 20:14:37 +0200

    Add backend code to repo

---
+++ backend/main.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const formidable = require('formidable');
+const fs = require("fs").promises
+
+const app = express();
+app.use(express.static('static'))
+app.use(express.json());
+const PORT = process.env.PORT || 8000;
+const uploadDir = __dirname + "/static/upload"
+
+app.get('/', () => {
+
+})
+app.post('/upload', (req, res, next) => {
+  const form = formidable({ multiples: true, uploadDir, keepExtensions: true });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.send("ok");
+    console.log({ fields, files });
+  });
+});
+//    getPathFromName
+const gpfn = name => uploadDir + "/" + name
+app.post('/delete-files', async (req, res) => {
+  for (const n of req.body.names) {
+    await fs.unlink(gpfn(n))
+  }
+  res.send("ok")
+})
+app.post('/rename-file', async (req, res) => {
+  let { nn, on } = req.body
+  if (!nn.includes('.')) nn += '.' + on.split('.').pop()
+  await fs.rename(gpfn(on), gpfn(nn))
+  res.send("ok")
+})
+app.get('/files', async (_, res) => {
+  res.json(await fs.readdir(__dirname + '/static/upload'))
+});
+app.listen(PORT, () => {
+  console.log("Server listening on port " + PORT);
+})
+
---
+++ backend/static/index.js
@@ -0,0 +1,59 @@
+const newDiv = `<!-- <div class="dImg"> -->
+      <p>$name</p><br>
+      <img src="upload/$name" alt="$name" width="120" height="120"/>
+      <button type="button" onclick="deleteFiles(['$name'])">Delete</button>
+      <button type="button" onclick="renameFile('$name')">Rename</button><br>
+      <label>
+        Select: 
+        <input type="checkbox" name="$name" />
+      </label>
+    <!-- </div> -->`
+
+function selectAll() {
+  for (const c of document.querySelectorAll("input[type='checkbox']")) {
+    c.checked = !c.checked
+  }
+}
+async function deleteFiles(names) {
+  if (!confirm("Are you sure about that file deleting thingy?")) return;
+  await fetch("/delete-files", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ names }),
+  })
+  getFiles()
+}
+async function removeAllSel() {
+  const names = []
+  for (const c of [...document.querySelectorAll("input[type='checkbox']")].filter(i => i.checked)) {
+    names.push(c.name)
+  }
+  deleteFiles(names)
+}
+async function renameFile(on) {
+  const nn = prompt("What will be the new name of this beautiful picture?")
+  if (!nn) return;
+  await fetch("/rename-file", {
+    method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ nn, on }),
+  })
+  getFiles()
+}
+const cont = document.getElementById("container");
+async function getFiles() {
+  const files = await fetch("/files").then(res => res.json());
+  for (const d of document.querySelectorAll(".dImg")) d.remove()
+  for (const f of files) {
+    const div = document.createElement("div")
+    div.className = "dImg"
+    div.innerHTML = newDiv.replaceAll("$name", f);
+    cont.appendChild(div)
+  }
+}
+getFiles()
+
